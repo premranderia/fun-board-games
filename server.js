3e245a43aac089627a6d1ae99a579d7c7a99b782
Prem Randeria <pranderia@marinsoftware.com>
2018-03-15 15:16:47 -0700

    Celnal up

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //Install express server
+const proxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.use('/api', proxy('https://code-name-node-server.herokuapp.com'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
