4310f99241c4948a97a6c63f83fd7ccc07e83cc7
Prem Randeria <pranderia@marinsoftware.com>
2018-03-16 22:17:58 -0700

    Fix unknown routing issue

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const proxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
