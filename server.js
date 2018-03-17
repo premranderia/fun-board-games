b7a4821ba86892ccf926a2f39b401db4d1cc0209
Prem Randeria <pranderia@marinsoftware.com>
2018-03-16 22:42:53 -0700

    fix routing

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const app = express();
 
 const path = require('path');
 
-app.get('/*', function(req, res) {
+app.all('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.get('/game', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// app.use('/api', proxy('https://code-name-node-server.herokuapp.com'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
