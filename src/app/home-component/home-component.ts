10df892270c6441c30d83dd10453b0ac9b2b123c
Prem Randeria <prem1890@gmail.com>
2020-07-06 22:18:55 -0700

    Fix Gujju Version (#8)

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -16,6 +16,7 @@ import { ROUTES } from '../routes/route.constant';
 export class HomeComponent implements OnInit {
   public gameId: number;
   public spyMode: boolean;
+  public gujVersion: boolean;
 
   constructor(private router: Router) { }
 
@@ -31,6 +32,9 @@ export class HomeComponent implements OnInit {
     if (this.spyMode) {
       queryParams.spy = this.spyMode;
     }
+    if (this.gujVersion) {
+      queryParams.gujVersion = this.gujVersion;
+    }
     this.navigate(queryParams);
   }
 
@@ -43,4 +47,5 @@ export class HomeComponent implements OnInit {
 export interface QueryParams {
   id?: number;
   spy?: boolean;
+  gujVersion?: boolean;
 }
