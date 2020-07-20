fbdab0b5286eb20e6bbefa38afd669a66903553d
Prem Randeria <pranderia@marinsoftware.com>
2020-07-20 13:27:32 -0700

    clle

--- src/app/linkee/linkee-game.component.ts
+++ src/app/linkee/linkee-game.component.ts
@@ -244,10 +244,10 @@ export class LinkeeGameComponent implements OnInit {
 }
 
 export interface LinkeeGameData {
-  id: number;
+  id?: number;
   players?: Players[];
   currentCard: number;
-  cards: Cards[];
+  cards?: Cards[];
   gameView?: GameView;
 }
 
